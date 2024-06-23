3ccf4f275e99a1e4be0ed932e5ba52e1682836a2
Moeki Kawakami <moeki.kawakami@icloud.com>
2024-06-23 21:24:42 +0900

    Fix

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import axios from "axios";
 import OpenAI from "openai";
 import * as readline from "readline";
 import { search } from "fast-fuzzy";
@@ -10,15 +11,15 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-interface CosensePage {
+export interface CosensePage {
   id: number;
   title: string | null;
   created: number;
   updated: number;
-  lines: Array<string>;
+  content?: string;
 }
 
-interface CosenseProject {
+export interface CosenseProject {
   name: string;
   displayName: string;
   exported: number;
@@ -28,41 +29,62 @@ interface CosenseProject {
 
 class CosenseData {
   pages: CosensePage[];
+  projectName: string;
 
-  constructor(jsonData: CosenseProject) {
-    this.pages = jsonData.pages;
+  constructor(projectName: string) {
+    this.pages = [];
+    this.projectName = projectName;
   }
 
-  get(pageName: string): string {
-    const page = this.pages.find((p) =>
-      p.lines.join("\n").toLowerCase().includes(pageName.toLowerCase())
-    );
-    return page ? page.lines.join("\n") : `Page "${pageName}" not found.`;
-  }
-
-  search(query: string): CosensePage {
+  async search(query: string): Promise<CosensePage> {
     const results = search(query, this.pages, {
       keySelector: (obj) => obj.title,
     });
+    const result = results[0];
 
-    return results[0];
+    if (!result) {
+      return null;
+    }
+
+    result.content = await fetchCosensePage(this.projectName, result.title);
+    return result;
   }
 }
 
-let cosenseData: CosenseData;
 let messages: Array<any> = [];
 const exploredPages = new Array<CosensePage>();
 let queries = [];
 
-async function loadJsonFile(filePath: string): Promise<void> {
-  try {
-    const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    cosenseData = new CosenseData(jsonData);
-    console.log("JSON file loaded successfully.");
-  } catch (error) {
-    console.error("Error loading JSON file:", error);
-    process.exit(1);
+async function loadJsonFile(filePath: string): Promise<CosenseData> {
+  const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+  const cosenseData = new CosenseData(jsonData.name);
+  cosenseData.pages = jsonData.pages;
+  return cosenseData;
+}
+
+async function fetchCosense(projectName: string): Promise<CosenseData> {
+  let skip = 0;
+  let cosenseData = new CosenseData(projectName);
+  while (true) {
+    const url = `https://scrapbox.io/api/pages/${projectName}?limit=1000&skip=${skip}`;
+    skip += 1000;
+
+    const response = await axios.get(url);
+    cosenseData.pages = [...response.data.pages, ...cosenseData.pages];
+    if (skip > response.data.count || skip > 5000) {
+      break;
+    }
   }
+  return cosenseData;
+}
+
+async function fetchCosensePage(
+  projectName: string,
+  pageName: string
+): Promise<string> {
+  const url = `https://scrapbox.io/api/pages/${projectName}/${pageName}/text`;
+  const response = await axios.get(url);
+  return response.data;
 }
 
 async function completion(prompt: string): Promise<string | null> {
@@ -78,49 +100,47 @@ async function completion(prompt: string): Promise<string | null> {
 async function askChatGPT(prompt: string): Promise<string | null> {
   try {
     messages.push({ role: "user", content: prompt });
-    const stream = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+    const chatCompletion = openai.chat.completions.create({
       messages,
-      stream: true,
+      model: "gpt-3.5-turbo",
     });
-    let content = "";
-    for await (const chunk of stream) {
-      const c = chunk.choices[0]?.delta?.content || "";
-      content += c;
-      process.stdout.write(c);
-    }
-    messages.push({ role: "assistant", content });
+
+    const message = (await chatCompletion).choices[0].message;
+    messages.push({ role: "assistant", content: message.content });
+    console.log(message.content);
   } catch (error) {
     console.error("Error calling ChatGPT API:", error);
     return null;
   }
 }
 
-async function explore(question: string) {
-  function explorePage(
-    pageTitle: string,
-    resume: boolean = false
-  ): Promise<void> {
-    if (!resume && exploredPages.find((p) => p.title === pageTitle)) return;
-    const page = cosenseData.search(pageTitle);
+async function explorePage(
+  pageTitle: string,
+  resume: boolean = false,
+  cosenseData: CosenseData
+): Promise<void> {
+  // if (!resume && exploredPages.find((p) => p.title === pageTitle)) return;
+  const page = await cosenseData.search(pageTitle);
 
-    if (!page) {
-      return;
-    }
-
-    if (exploredPages.find((p) => p.title === page.title)) return;
-    exploredPages.push(page);
-
-    if (exploredPages.length > 5) {
-      return;
-    }
-
-    const links = page.lines.join("\n").match(/\[([^\]]+)\]/g) || [];
-    for (const link of links) {
-      explorePage(link.slice(1, -1));
-    }
+  if (!page) {
+    return;
   }
 
+  if (exploredPages.find((p) => p.title === page.title)) return;
+  exploredPages.push(page);
+  console.log(`* ${page.title}`);
+
+  if (exploredPages.length > 5) {
+    return;
+  }
+
+  const links = page.content.match(/\[([^\]]+)\]/g) || [];
+  for await (const link of links) {
+    await explorePage(link.slice(1, -1), resume, cosenseData);
+  }
+}
+
+async function walk(question: string, cosenseData: CosenseData) {
   let result;
   let tries = 0;
   while (true) {
@@ -141,16 +161,21 @@ async function explore(question: string) {
 
     Search query:`);
     console.log(`Query: ${query}`);
+    console.log("Sources:");
     if (query.match("null")) {
-      const resumePageTitle = exploredPages[exploredPages.length - 1].title;
+      const resumePage = exploredPages[exploredPages.length - 1];
+      if (!resumePage) {
+        break;
+      }
+      const resumePageTitle = resumePage.title;
       exploredPages.splice(0, 5);
-      explorePage(resumePageTitle, true);
+      await explorePage(resumePageTitle, true, cosenseData);
     }
     if (query === queries[queries.length - 1]) {
       break;
     }
     queries.push(query);
-    result = cosenseData.search(query);
+    result = await cosenseData.search(query);
     if (result?.title) {
       break;
     }
@@ -160,24 +185,24 @@ async function explore(question: string) {
     tries += 1;
   }
   if (result) {
-    explorePage(result.title);
-    console.log("Search results:");
-    exploredPages.forEach((r) => console.log(`* ${r.title}`));
+    await explorePage(result.title, false, cosenseData);
   }
   console.log("---");
 
   await askChatGPT(
-    `Based on the following context and the initial question "${question}", provide a comprehensive answer:\n\n${exploredPages
+    `Based on hisotry of this conversation and the following context and the initial question "${question}", provide a comprehensive answer:\n\n${exploredPages
       .map((p) => {
         return [
           "----",
           `title: ${p.title}`,
-          `content: ${p.lines.slice(50).join("\n")}`,
+          `content: ${p.content}`,
           "----",
         ].join("\n");
       })
       .join("\n")}`
   );
+
+  console.log(messages);
 }
 
 const rl = readline.createInterface({
@@ -193,8 +218,13 @@ async function cliLoop(): Promise<void> {
   console.log("Welcome to the Cosense ChatGPT Explorer!");
   console.log("Type your questions or 'exit' to quit.");
 
-  const jsonFilePath = process.env.COSENSE_DATA_PATH;
-  await loadJsonFile(path.resolve(jsonFilePath));
+  let cosenseData;
+  if (process.argv[2]) {
+    cosenseData = await fetchCosense(process.argv[2]);
+  } else {
+    const jsonFilePath = process.env.COSENSE_DATA_PATH;
+    cosenseData = await loadJsonFile(path.resolve(jsonFilePath));
+  }
 
   while (true) {
     const question = await askQuestion("\n> ");
@@ -205,8 +235,10 @@ async function cliLoop(): Promise<void> {
       break;
     }
 
-    await explore(question);
+    await walk(question, cosenseData);
   }
 }
 
 cliLoop().catch(console.error);
+
+export { fetchCosense, walk, loadJsonFile };
