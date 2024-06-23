33d363f1f4d9c2bc731b7b9fb8ea43c913f1bd05
Moeki Kawakami <moeki.kawakami@icloud.com>
2024-06-23 22:44:58 +0900

    Fix

--- lib/fetchCosense.ts
+++ lib/fetchCosense.ts
@@ -4,11 +4,9 @@ import { CosenseData } from ".";
 export async function fetchCosense(projectName: string): Promise<CosenseData> {
   let skip = 0;
   let cosenseData = new CosenseData(projectName);
-  process.stdout.write("loading all pages");
   while (true) {
     const url = `https://scrapbox.io/api/pages/${projectName}?limit=1000&skip=${skip}`;
     skip += 1000;
-    process.stdout.write(".");
 
     const response = await axios.get(url);
     cosenseData.pages = [...response.data.pages, ...cosenseData.pages];
@@ -16,6 +14,5 @@ export async function fetchCosense(projectName: string): Promise<CosenseData> {
       break;
     }
   }
-  console.log("");
   return cosenseData;
 }
