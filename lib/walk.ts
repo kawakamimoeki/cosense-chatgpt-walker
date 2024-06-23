9bac86f41838823626adc090d75189fc761cf811
Moeki Kawakami <moeki.kawakami@icloud.com>
2024-06-23 22:46:44 +0900

    Fix

--- lib/walk.ts
+++ lib/walk.ts
@@ -56,7 +56,6 @@ export async function walk(
   if (result) {
     await explorePage(result.title, false, cosenseData, exploredPages);
   }
-  console.log("---");
 
   const res = await askChatGPT(
     `Based on hisotry of this conversation and the following context and the initial question "${question}", provide a comprehensive answer:\n\n${exploredPages
