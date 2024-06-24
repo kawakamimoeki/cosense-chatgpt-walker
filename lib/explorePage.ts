eb727e0a502681fc386841d2330d9a034679c9ed
Moeki Kawakami <moeki.kawakami@icloud.com>
2024-06-24 18:49:24 +0900

    Fix

--- lib/explorePage.ts
+++ lib/explorePage.ts
@@ -4,10 +4,12 @@ export async function explorePage(
   pageTitle: string,
   resume: boolean = false,
   cosenseData: CosenseData,
-  exploredPages: Array<CosensePage>
+  exploredPages: Array<CosensePage>,
+  depth: number = 0,
 ): Promise<void> {
   if (!resume && exploredPages.find((p) => p.title === pageTitle)) return;
   if (exploredPages.length > 10) return;
+  if (depth > 2) return;
   const page = await cosenseData.search(pageTitle);
 
   if (!page) {
@@ -20,9 +22,9 @@ export async function explorePage(
   exploredPages.push(page);
 
   for await (const link of page.relatedPages.links1hop) {
-    await explorePage(link.title, resume, cosenseData, exploredPages);
+    await explorePage(link.title, resume, cosenseData, exploredPages, depth + 1);
   }
   for await (const link of page.relatedPages.links2hop) {
-    await explorePage(link.title, resume, cosenseData, exploredPages);
+    await explorePage(link.title, resume, cosenseData, exploredPages, depth + 1);
   }
 }
